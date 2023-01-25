815a759a729f8181658b60a36fbdbaeb192ae99e
upmoris <upmoris@gmail.com>
2023-01-25 22:59:58 +0400

    optimise horoscope scrape

--- server/scrape_horoscopes.ts
+++ server/scrape_horoscopes.ts
@@ -1,32 +1,8 @@
-import {
-  getHoroscopeTomorrowCareer,
-  getHoroscopeTomorrowGeneral,
-  getHoroscopeTomorrowLove,
-  getHoroscopeTomorrowWellness,
-} from "./services/horoscope";
 import fs from "fs/promises";
+import { getHoroscopeTomorrowAllCategories } from "./services/horoscope";
 
 (async () => {
-  const horoscopes_general = await getHoroscopeTomorrowGeneral();
-  const horoscopes_love = await getHoroscopeTomorrowLove();
-  const horoscopes_career = await getHoroscopeTomorrowCareer();
-  const horoscopes_wellness = await getHoroscopeTomorrowWellness();
-
-  await fs.writeFile(
-    "output/horoscopes_general.json",
-    JSON.stringify(horoscopes_general)
-  );
-  await fs.writeFile(
-    "output/horoscopes_love.json",
-    JSON.stringify(horoscopes_love)
-  );
-  await fs.writeFile(
-    "output/horoscopes_career.json",
-    JSON.stringify(horoscopes_career)
-  );
-  await fs.writeFile(
-    "output/horoscopes_wellness.json",
-    JSON.stringify(horoscopes_wellness)
-  );
+  const horoscope_data = await getHoroscopeTomorrowAllCategories();
+  await fs.writeFile("output/horoscope.json", JSON.stringify(horoscope_data));
   console.log("Horoscopes scraped successfully..");
 })();
--- server/services/horoscope.ts
+++ server/services/horoscope.ts
@@ -1,76 +1,15 @@
 import axios from "axios";
 import { Horoscope } from "../types";
-import { trimToSentence } from "../utils";
 require("dotenv").config();
 
 const api_url = `${process.env.HOROSCOPE_API_URL}:3002`;
-const character_limit = 980;
 
-async function getHoroscopeTomorrowGeneral(): Promise<Horoscope[]> {
-  let horoscopes: Horoscope[] = (await axios.get(`${api_url}/tomorrow-general`))
-    .data.data;
-  horoscopes = horoscopes.map((h) => {
-    return {
-      ...h,
-      horoscope: trimToSentence(`In short, ${h.horoscope}`, character_limit),
-    };
-  });
-
-  return horoscopes;
-}
-
-async function getHoroscopeTomorrowLove(): Promise<Horoscope[]> {
-  let horoscopes: Horoscope[] = (await axios.get(`${api_url}/tomorrow-love`))
-    .data.data;
-  horoscopes = horoscopes.map((h) => {
-    return {
-      ...h,
-      horoscope: trimToSentence(
-        `Regarding love, ${h.horoscope}`,
-        character_limit
-      ),
-    };
-  });
-
-  return horoscopes;
-}
-
-async function getHoroscopeTomorrowCareer(): Promise<Horoscope[]> {
-  let horoscopes: Horoscope[] = (await axios.get(`${api_url}/tomorrow-career`))
-    .data.data;
-  horoscopes = horoscopes.map((h) => {
-    return {
-      ...h,
-      horoscope: trimToSentence(
-        `Regarding career, ${h.horoscope}`,
-        character_limit
-      ),
-    };
-  });
-
-  return horoscopes;
-}
-
-async function getHoroscopeTomorrowWellness(): Promise<Horoscope[]> {
-  let horoscopes: Horoscope[] = (
-    await axios.get(`${api_url}/tomorrow-wellness`)
+async function getHoroscopeTomorrowAllCategories(): Promise<Horoscope[]> {
+  const horoscopes: Horoscope[] = (
+    await axios.get(`${api_url}/tomorrow-merged`)
   ).data.data;
-  horoscopes = horoscopes.map((h) => {
-    return {
-      ...h,
-      horoscope: trimToSentence(
-        `Regarding health, ${h.horoscope}`,
-        character_limit
-      ),
-    };
-  });
 
   return horoscopes;
 }
 
-export {
-  getHoroscopeTomorrowGeneral,
-  getHoroscopeTomorrowLove,
-  getHoroscopeTomorrowCareer,
-  getHoroscopeTomorrowWellness,
-};
+export { getHoroscopeTomorrowAllCategories };
