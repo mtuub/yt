50c089ced1f8bdd76929a18fa1e1309afb6e48b1
upmoris <upmoris@gmail.com>
2023-01-31 22:18:25 +0400

    -

--- server/scrape_horoscopes.ts
+++ server/scrape_horoscopes.ts
@@ -3,6 +3,9 @@ import { getHoroscopeTomorrowAllCategories } from "./services/horoscope";
 
 (async () => {
   const horoscope_data = await getHoroscopeTomorrowAllCategories();
-  await fs.writeFile("output/horoscope.json", JSON.stringify(horoscope_data));
+  await fs.writeFile(
+    "output/horoscope.json",
+    JSON.stringify(horoscope_data.slice(0, 1)) // TODO: remove slice + refresh pictory token
+  );
   console.log("Horoscopes scraped successfully..");
 })();
