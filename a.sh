92d57a165e860f658e8cf97d1460691c0efc0a29
upmoris <upmoris@gmail.com>
2023-02-03 17:04:44 +0400

    -

--- a.sh
+++
@@ -1,6 +0,0 @@
-name=Horoscope
-value=$(echo "The following compositions are available:
-
-Horoscope     24      1920x1080      2304 (96.00 sec)
-Horoscope2    24      1920x1080      2880 (120.00 sec)" | awk -v name="Horoscope" '$1==name {print $4}')
-echo $value
