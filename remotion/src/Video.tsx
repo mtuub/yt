1999b7eebdbf81a5b2582f1eaf332b8e51e38009
upmoris <upmoris@gmail.com>
2023-02-03 15:19:14 +0400

    -

--- remotion/src/Video.tsx
+++ remotion/src/Video.tsx
@@ -15,16 +15,29 @@ export const RemotionVideo = () => {
     ].to;
 
   return (
-    <Composition
-      id="Horoscope"
-      component={HoroscopeComponent}
-      durationInFrames={24 * Math.round(video_duration + 3)}
-      fps={24}
-      height={1080}
-      width={1920}
-      defaultProps={{
-        horoscopeWithSubtitles,
-      }}
-    />
+    <>
+      <Composition
+        id="Horoscope"
+        component={HoroscopeComponent}
+        durationInFrames={24 * Math.round(video_duration + 3)}
+        fps={24}
+        height={1080}
+        width={1920}
+        defaultProps={{
+          horoscopeWithSubtitles,
+        }}
+      />
+      <Composition
+        id="Horoscope2"
+        component={HoroscopeComponent}
+        durationInFrames={30 * Math.round(video_duration + 3)}
+        fps={24}
+        height={1080}
+        width={1920}
+        defaultProps={{
+          horoscopeWithSubtitles,
+        }}
+      />
+    </>
   );
 };
