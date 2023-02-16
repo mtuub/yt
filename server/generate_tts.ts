f5241d226ce20a761f0cd5cd5bba7b8f03f94da2
upmoris <upmoris@gmail.com>
2023-02-16 22:48:12 +0400

    -

--- server/generate_tts.ts
+++ server/generate_tts.ts
@@ -3,21 +3,22 @@ import convertTTS from "./services/tts";
 import { Horoscope } from "./types";
 
 (async () => {
+  const sign = process.argv[2].toLowerCase();
+
   const horoscopes: Horoscope[] = JSON.parse(
     await fs.readFile("output/horoscope.json", "utf-8")
   );
+
+  const sign_horoscope = horoscopes.find((h) => h.sign === sign);
 
   const save_dir = "output/audios";
   try {
     await fs.mkdir(save_dir, { recursive: true });
   } catch (error) {}
 
-  for (let idx = 0; idx < horoscopes.length; idx++) {
-    const horoscope = horoscopes[idx];
-
-    await convertTTS(horoscope.horoscope, `${save_dir}/${horoscope.sign}.mp3`);
-    console.log(
-      `Generated audio for ${horoscope.sign} (${idx + 1}/${horoscopes.length})`
-    );
-  }
+  await convertTTS(
+    sign_horoscope.horoscope,
+    `${save_dir}/${sign_horoscope.sign}.mp3`
+  );
+  console.log(`Generated audio for ${sign_horoscope.sign}`);
 })();
